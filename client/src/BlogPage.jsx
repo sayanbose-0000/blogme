926e95447045494d5ffa1c06d76986d9a7fbe01c
sayanbose-0000 <bosesayan0000@gmail.com>
2024-04-26 22:35:00 +0530

    added stuffs

--- client/src/BlogPage.jsx
+++ client/src/BlogPage.jsx
@@ -3,13 +3,30 @@ import '../styles/blogpage.scss';
 const BlogPage = () => {
   return (
     <div className='blogpage'>
+
       <div className="blogcontainer">
-        <img src="person.jpg" className='blogimage' alt="blogimage" height={100} width={100} />
+        <div className="blogstart">
+          <div className="image_likes">
+            <img src="person.jpg" className="blogimg" alt="blogimg" height={100} width={100} />
+            <div className="likes">
+              <p className="likescount">10k</p>
+              <img src="heart_fill.svg" alt="heart" className="heart" height={100} width={100} />
+            </div>
+          </div>
+
+          <div className="author_date">
+            <p>Ram</p>
+            <p>13/1/22</p>
+          </div>
+        </div>
+
         <div className="blogcontent">
           <p>Hello</p>
         </div>
+
       </div>
     </div>
+
   );
 };
 
